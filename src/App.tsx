6cd835612f35a5bc43e0b534b47ce9d53d7f13c7
an_luu <luutrantruongan@gmail.com>
2023-11-24 05:17:57 +0700

    anluutrqn

---
+++ src/App.tsx
@@ -0,0 +1,27 @@
+import React from 'react'
+import { Routes, Route } from 'react-router-dom'
+import SigninForms from './_auth/forms/SigninForms'
+import SignupForms from './_auth/forms/SignupForms'
+import { Home } from './_root/pages'
+import AuthLayout from './_auth/AuthLayout'
+
+import './index.css'
+
+const App = () => {
+  return (
+    <main className='flex h-screen'>
+        <Routes>
+            <Route element={<AuthLayout />}>
+                <Route path='/sign-in' element={<SigninForms />} />
+                <Route path='/sign-up' element={<SignupForms />} />
+            </Route>
+
+            <Route>
+               <Route index element={<Home />} />
+            </Route>
+        </Routes>
+    </main>
+  )
+}
+
+export default App
